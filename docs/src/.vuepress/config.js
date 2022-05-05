7369bf8bbea5395742ec6b901ce3b97f31158c1d
Bukunmikuti@gmail.com <Bukunmikuti@gmail.com>
2022-05-05 19:12:59 +0100

    Added base to head

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -10,6 +10,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
+    ['base', { href: "https://docs--bibleup.netlify.app"}], 
     ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/assets/apple-touch-icon.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/assets/favicon-32x32.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/assets/favicon-16x16.png"}],
