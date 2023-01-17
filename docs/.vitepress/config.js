737e942218ea3e80f6d15e157b6e8780dffae4fb
Bukunmikuti <bukunmikuti@gmail.com>
2023-01-17 01:20:31 +0100

    add custom background css and fonts

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -12,10 +12,16 @@ export default {
   head: [
     // https://bibleup.netlify.app
     //['base', { href: "https://bibleup.netlify.app"}],
+    // fonts
+    ['link', {rel: "preconnect", href: "https://fonts.googleapis.com"}],
+    ['link', {rel: "preconnect", href: "https://fonts.gstatic.com", crossorigin: true}],
+    ['link', {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400&display=swap"}],
+    // favicons
     ['link', { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png"}],
     ['link', { rel: "icon", type: "image/x-icon", href: "/favicon.ico"}],
     ['link', { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"}],
     ['link', { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"}],
+    // meta theme
     ["meta", { name: "theme-color", content: "#fff" }],
     ["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
     ["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" },
